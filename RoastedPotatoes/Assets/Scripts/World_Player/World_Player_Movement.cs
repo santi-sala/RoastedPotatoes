d971fe1b72684bee2cacc9e2b2956a1954cf9c13
T1607 <mina.t.eriksson@gmail.com>
2023-05-15 10:42:55 +0300

    Testing_NoChanges

---
+++ RoastedPotatoes/Assets/Scripts/World_Player/World_Player_Movement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class World_Player_Movement : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        SetUpInputs();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetUpInputs()
+    {
+
+    }
+
+    void MoveUp()
+    {
+
+    }
+    void MoveDown()
+    {
+
+    }
+    void MoveLeft()
+    {
+
+    }
+    void MoveRight()
+    {
+
+    }
+}
